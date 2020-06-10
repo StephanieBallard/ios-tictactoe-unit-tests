ee957acd823716b85d7d21a7a6c6745b3ba6eab8
Stephanie Ballard <user.name>
2020-06-10 16:53:06 -0700

    unit tests for tic tac toe board completed and all green

---
+++ TicTacToeTests/GameBoardTests.swift
@@ -0,0 +1,69 @@
+//
+//  GameBoardTests.swift
+//  TicTacToeTests
+//
+//  Created by Stephanie Ballard on 6/10/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import XCTest
+@testable import TicTacToe
+
+class GameBoardTests: XCTestCase {
+
+    func testCreatingEmptyBoard() {
+        let board = GameBoard()
+        
+        for x in 0..<3 {
+            for y in 0..<3 {
+                XCTAssertNil(board[(x,y)])
+            }
+        }
+        
+        
+    }
+
+    func testPlacingMarks() {
+        var board = GameBoard()
+        
+        XCTAssertNoThrow(try board.place(mark: .o, on: (0, 0)))
+        XCTAssertEqual(board[(0,0)], .o)
+        XCTAssertNoThrow(try board.place(mark: .x, on: (2, 2)))
+        XCTAssertEqual(board[(2, 2)], .x)
+        
+        // This ensures all other spaces in the board are nil (empty)
+        for x in 0..<3  {
+            for y in 0..<3 {
+                if x == 0 && y == 0 { continue }
+                if x == 2 && y == 2 { continue }
+                XCTAssertNil(board[(x, y)])
+            }
+        }
+        
+        XCTAssertThrowsError(try board.place(mark: .x, on: (0, 0))) { error in
+            XCTAssertEqual(error as? GameBoardError, GameBoardError.invalidSquare)
+        }
+    }
+    
+    func testIsFull() {
+        var board = GameBoard()
+        try! board.place(mark: .x, on: (0, 0))
+        try! board.place(mark: .o, on: (0, 1))
+        try! board.place(mark: .x, on: (1, 0))
+        try! board.place(mark: .o, on: (2, 0))
+        try! board.place(mark: .x, on: (0, 2))
+        try! board.place(mark: .o, on: (1, 1))
+        try! board.place(mark: .x, on: (2, 1))
+        try! board.place(mark: .o, on: (1, 2))
+        XCTAssertFalse(board.isFull)
+        try! board.place(mark: .x, on: (2, 2))
+        XCTAssertTrue(board.isFull)
+        
+    }
+}
+
+//var array = [Int]()
+//// []
+//array[0] = 1
+//// [1]
+//print(array[0])
